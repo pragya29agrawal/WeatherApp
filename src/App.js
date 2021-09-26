ecad1800a70d1df3a31b9f980a956b217a39bafc
pragya29agrawal <pragyaagrawal2910@gmail.com>
2021-09-26 19:53:22 +0530

    first commit of weather app project

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
+import Forecast from './components/forecast';
+export default function App() {
+  
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+  const [dataForecast, setDataForecast] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+     
+
+      // await fetch(`${process.env.REACT_APP_API_URL}/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      // .then(res => res.json())
+      // .then(resultforecast => {
+      //   setDataForecast(resultforecast)
+      //   console.log(resultforecast);
+      // });
+    }
+    fetchData();
+  }, [lat,long])
+  
+  return (
+    <div className="App">
+      {(typeof data.main != 'undefined') ? (
+        <div>
+        <Weather weatherData={data}/>
+        {/* <Forecast forecast = {dataForecast}/> */}
+        </div>
+      ): (
+        <div></div>
+      )}
+      
+    </div>
+  );
+}
